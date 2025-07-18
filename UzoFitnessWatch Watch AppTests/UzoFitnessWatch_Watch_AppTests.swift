98e40da29c35c745e1e02f5e7be4abaf3029a423
kosiuzo <KosiUzodinma@gmail.com>
2025-07-18 13:03:13 -0400

    Setting up the Target for Watch app, added logo and updated the entitlement for the app group

---
+++ UzoFitnessWatch Watch AppTests/UzoFitnessWatch_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  UzoFitnessWatch_Watch_AppTests.swift
+//  UzoFitnessWatch Watch AppTests
+//
+//  Created by Kosi Uzodinma on 7/18/25.
+//
+
+import Testing
+@testable import UzoFitnessWatch_Watch_App
+
+struct UzoFitnessWatch_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
